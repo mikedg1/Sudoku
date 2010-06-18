eecefbcef375fe4e3ad0416b11a723a1895f31d6
Michael DiGovanni <michaeldigiovanni@Michael-DiGovannis-MacBook-Pro.local>
2010-06-18 19:43:29 -0400

    Initial public Sudoku

---
+++ dgSudoku/src/com/mikedg/android/sudoku/SudokuActivity.java
@@ -0,0 +1,8 @@
+package com.mikedg.android.sudoku;
+
+import android.app.Activity;
+
+public class SudokuActivity extends Activity {
+	public static final String PREF_TIMES_VIEWED = "ptv";
+    	 
+}
